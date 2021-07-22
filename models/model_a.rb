eabb953380bc437a17f76620df10849b7b0a9c32
Sam Devine <samuel.will.devine@gmail.com>
2021-07-21 21:15:01 -0400

    Add school to model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  name: "Best School Ever",
+  mission: "To teach everyone how to read and do math and stuff",
+  num_students: 101,
+  num_teachers: 10,
+  is_air_conditioned: true,
+  is_a_good_school: true,
+  first_graders: ["George", "Mallory", "Katie"],
+  sports_teams: ["Soccer", "Basketball", "Swimming"]
+}
