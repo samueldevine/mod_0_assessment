7291fe001675732e11566502fb92a62b5df36bcb
Sam Devine <samuel.will.devine@gmail.com>
2021-07-21 21:19:52 -0400

    Add cat to model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+cat = {
+  name: "Barley",
+  color: "Orange",
+  age: 2,
+  weight: 10,
+  is_hungry: true,
+  is_sleepy: false,
+  favorite_toys: ["mouse", "bird on a string", "twist ties"],
+  nicknames: ["Barles", "Chonky Boi", "Little Man"]
+}
