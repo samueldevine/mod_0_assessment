eb931e23f17cfb87478893917f26871c287b56bd
Sam Devine <samuel.will.devine@gmail.com>
2021-07-21 21:09:08 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+famous_person = "Jack Black"
+is_active = true
+
+famous_person = "Martha Stewart"
+
+num_3 = 3
+num_9 = 9
+sum = num_3 + num_9
+
+animals = ["tiger", "lion", "elephant"]
+
+animals.shift
+famous_person.length
